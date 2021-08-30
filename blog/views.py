65de4e5dcc9452588dadd77bdc55e39d1d06d899
Radek <radoslaw.piekarski@gmail.com>
2021-08-30 09:22:17 +0200

    mocking post data in views.py

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.http import Http404
-# Create your views here.
 
+# Create your views here.
+post = {
+    "slug": "hike-in-the-mountains",
+    "image": "mountains.jpg",
+    "author": "Radek",
+    "date": date(2021, 09, 29),
+    "title" "Mountain Hiking",
+    "excerpt": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nec ligula magna. Praesent aliquet rhoncus massa, blandit rhoncus mauris malesuada in."
+    "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nec ligula magna. Praesent aliquet rhoncus massa, blandit rhoncus mauris malesuada in. Sed vulputate, orci id varius rutrum, eros nunc mollis nunc, vitae ornare sem orci sed nisi."
+}
 
 def index(request):
     return render(request, "blog/index.html")
