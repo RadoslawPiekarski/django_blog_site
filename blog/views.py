792c0e85a9d21f111fd003679b0ffa8b15e9df04
Radek <radoslaw.piekarski@gmail.com>
2021-08-30 09:43:37 +0200

    mocking post data in views.py / changed post.html

--- blog/views.py
+++ blog/views.py
@@ -2,28 +2,37 @@ from django.shortcuts import render
 from django.http import Http404
 
 # Create your views here.
-post = [
-{
-    "slug": "hike-in-the-mountains",
-    "image": "mountains.jpg",
-    "author": "Radek",
-    "date": date(2021, 09, 29),
-    "title" "Mountain Hiking",
-    "excerpt": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nec ligula magna. Praesent aliquet rhoncus massa, blandit rhoncus mauris malesuada in.",
-    "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nec ligula magna. Praesent aliquet rhoncus massa, blandit rhoncus mauris malesuada in. Sed vulputate, orci id varius rutrum, eros nunc mollis nunc, vitae ornare sem orci sed nisi."
-},
-{
-    "slug": "second-post",
-    "image": "mountains.jpg",
-    "author": "Radek",
-    "date": date(2021, 09, 23),
-    "title" "Second post",
-    "excerpt": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nec ligula magna. Praesent aliquet rhoncus massa, blandit rhoncus mauris malesuada in.",
-    "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nec ligula magna. Praesent aliquet rhoncus massa, blandit rhoncus mauris malesuada in. Sed vulputate, orci id varius rutrum, eros nunc mollis nunc, vitae ornare sem orci sed nisi."
-},
+posts = [
+    {
+        "slug": "hike-in-the-mountains",
+        "image": "mountains.jpg",
+        "author": "Radek",
+        "date": date(2021, 09, 29),
+        "title" "Mountain Hiking",
+        "excerpt": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nec ligula magna. Praesent aliquet rhoncus massa, blandit rhoncus mauris malesuada in.",
+        "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nec ligula magna. Praesent aliquet rhoncus massa, blandit rhoncus mauris malesuada in. Sed vulputate, orci id varius rutrum, eros nunc mollis nunc, vitae ornare sem orci sed nisi."
+    },
+    {
+        "slug": "second-post",
+        "image": "mountains.jpg",
+        "author": "Radek",
+        "date": date(2021, 09, 23),
+        "title" "Second post",
+        "excerpt": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nec ligula magna. Praesent aliquet rhoncus massa, blandit rhoncus mauris malesuada in.",
+        "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nec ligula magna. Praesent aliquet rhoncus massa, blandit rhoncus mauris malesuada in. Sed vulputate, orci id varius rutrum, eros nunc mollis nunc, vitae ornare sem orci sed nisi."
+    },
 ]
+
+def get_date(post):
+    return post['date']
+
+
 def index(request):
-    return render(request, "blog/index.html")
+    sorted_posts = posts.sort(key=get_date)
+    latest_posts = sorted_posts[-3:]
+    return render(request, "blog/index.html", {
+        "posts": latest_posts
+    })
 
 
 def posts(request):
