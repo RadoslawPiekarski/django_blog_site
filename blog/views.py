b7d958214d522cca469c4e055989059d58a35b45
Radek <radoslaw.piekarski@gmail.com>
2021-09-01 21:43:46 +0200

    refactored post path

--- blog/views.py
+++ blog/views.py
@@ -39,10 +39,15 @@ def index(request):
 
 
 def posts(request):
-    return render(request, "blog/all_posts.html")
+    return render(request, "blog/all_posts.html", {
+        "all_posts": all_posts
+    })
 
 
 # single post
 
 def post_detail(request, slug):
-    return render(request, "blog/post_detail.html")
+    identfied_post = next(post for post in all_posts if post['slug'] == slug)
+    return render(request, "blog/post_detail.html", {
+        "post": identfied_post
+    })
