1403b17355ac7140b36ac229ba620a7f9b704a28
Radek <radoslaw.piekarski@gmail.com>
2021-09-15 21:47:03 +0200

    adjusted Author, Tag, Post models

--- blog/models.py
+++ blog/models.py
@@ -1,24 +1,25 @@
 from django.db import models
-
+from django.core.validators import MinLengthValidator
 
 # Create your models here.
 
+
 class Author(models.Model):
-    first_name = models.CharField(max_length=15)
+    first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     email_address = models.EmailField(max_length=20)
 
 
 class Tag(models.Model):
-    caption = models.CharField(max_length=15)
+    caption = models.CharField(max_length=20)
 
 
 class Post(models.Model):
     title = models.CharField(max_length=50)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, related_name="posts")
     excerpt = models.CharField(max_length=256)
-    image_name = models.CharField(max_length=20)
-    date = models.DateField
-    slug = models.SlugField
-    content = models.TextField
-    tag = models.ManyToManyField(Tag)
+    image_name = models.CharField(max_length=100)
+    date = models.DateField(auto_now=True)
+    slug = models.SlugField(unique=True)
+    content = models.TextField(validators=[MinLengthValidator(10)])
+    tags = models.ManyToManyField(Tag)
