dcc6e9275ed76d337fac1b4ef0136338c781b199
Radek <radoslaw.piekarski@gmail.com>
2021-09-15 22:09:14 +0200

    added text representation for Author

--- blog/models.py
+++ blog/models.py
@@ -9,6 +9,9 @@ class Author(models.Model):
     last_name = models.CharField(max_length=20)
     email_address = models.EmailField(max_length=50)
 
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
 
 class Tag(models.Model):
     caption = models.CharField(max_length=20)
