cf62e6a7597a276269885448e74cf18a770e399f
Radek <radoslaw.piekarski@gmail.com>
2021-09-26 15:23:35 +0200

    changed str repr in author

--- blog/models.py
+++ blog/models.py
@@ -13,7 +13,7 @@ class Author(models.Model):
         return f"{self.first_name} {self.last_name}"
 
     def __str__(self):
-        return f"{self.full_name}"
+        return self.full_name()
 
 
 class Tag(models.Model):
