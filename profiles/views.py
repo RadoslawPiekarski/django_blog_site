f142e9da1bdb95bd19b9a21e1a225e8e7694c26f
Radek <radoslaw.piekarski@gmail.com>
2021-11-11 13:43:08 +0100

    added helper function store_file

--- profiles/views.py
+++ profiles/views.py
@@ -3,11 +3,15 @@ from django.views import View
 from django.http import HttpResponseRedirect
 # Create your views here.
 
+def store_file(file):
+    with open("temp/image.jpg", "wb+") as dest:  #wb+ -> pliki binarne
+        for chunk in file.chunks()  # czyta plik partiami aby nie zajmować dużej ilości pamięci
+            dest.write(chunk) # zapisuje kawałek pliku
 
 class CreateProfileView(View):
     def get(self, request):
         return render(request, "profiles/create_profile.html")
 
     def post(self, request):
-        print(request.FILES["image"])
+        store_file(request.FILES["image"])
         return HttpResponseRedirect("/profiles")
