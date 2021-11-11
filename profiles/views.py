371a017b7396d67a6233ae014c40a1fa6f6fa44f
Radek <radoslaw.piekarski@gmail.com>
2021-11-11 18:14:52 +0100

    PEP8

--- profiles/views.py
+++ profiles/views.py
@@ -4,10 +4,12 @@ from django.http import HttpResponseRedirect
 from forms import ProfileForm
 # Create your views here.
 
+
 def store_file(file):
     with open("temp/image.jpg", "wb+") as dest:  # wb+ -> pliki binarne
         for chunk in file.chunks():  # czyta plik partiami aby nie zajmować dużej ilości pamięci
             dest.write(chunk)  # zapisuje kawałek pliku
+
 
 class CreateProfileView(View):
     def get(self, request):
