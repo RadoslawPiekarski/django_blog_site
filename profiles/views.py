f0e6e15b41bcb6b9bb89a0a5b332a4914a6620cb
Radek <radoslaw.piekarski@gmail.com>
2021-11-12 21:53:27 +0100

    commented non used code

--- profiles/views.py
+++ profiles/views.py
@@ -24,12 +24,12 @@ class CreateProfileView(CreateView):
 #
 #     def post(self, request):
 #         submitted_form = ProfileForm(request.POST, request.FILES)
-
-        if submitted_form.is_valid():
-            profile = UserProfile(image=request.FILES["user_image"])
-            profile.save()
-            return HttpResponseRedirect("/profiles")
-
-        return render(request, "profiles/create_profile.html", {
-            "form": submitted_form
-        })
+#
+#         if submitted_form.is_valid():
+#             profile = UserProfile(image=request.FILES["user_image"])
+#             profile.save()
+#             return HttpResponseRedirect("/profiles")
+#
+#         return render(request, "profiles/create_profile.html", {
+#             "form": submitted_form
+#         })
