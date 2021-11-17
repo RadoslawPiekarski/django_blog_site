8a572b86421a833bc980eec3c5a64bc4fd5b8bfb
Radek <radoslaw.piekarski@gmail.com>
2021-11-18 00:24:38 +0100

    PEP8s

--- django_blog_site/urls.py
+++ django_blog_site/urls.py
@@ -22,4 +22,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('profiles/', include('profiles.urls')),
-]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
